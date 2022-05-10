f05ca64a0cfc775fe0ca4c5602c72d1261c0e45a
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-10 14:58:28 +0530

    [shravanthi] -Added Nodes at Last in Simple LL

---
+++ SimpleLinkedListOperations/SimpleLinkedList1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLinkedListOperations
+{
+    public class SimpleLinkedList1<T>
+    {
+        public int count = 0;
+        Node<T> head;
+        public void AddNodeToLast(T Element)
+        {
+            Node<T> newNode = new Node<T>(Element);
+
+            //2. assign data element
+            newNode.data = Element;
+            //3. assign null to the next of new node
+            newNode.next = null;
+            //4. Check the Linked List is empty or not,
+            //   if empty make the new node as head
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                //5. Else, traverse to the last node
+                Node<T> temp = new Node<T>(Element);
+                temp = head;
+                while (temp.next != null)
+                    temp = temp.next;
+
+                //6. Change the next of last node to new node
+                temp.next = newNode;
+            }
+            count++;
+        }
+
+        public void display()
+        {
+            Node<T> temp = head;
+            while (temp != null)
+            {
+                Console.Write("->{0}", temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
